5fd1a045760522eca02e7c23b2aca8d82d1628eb
Dan Piet <daniel.piet@rokt.com>
2024-03-11 22:15:53 -0400

    fz

--- roles/dotfiles/files/config.lua
+++ roles/dotfiles/files/config.lua
@@ -4,4 +4,5 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'preservim/nerdtree'
   use "morhetz/gruvbox"
+  use { 'junegunn/fzf.vim', requires = 'junegunn/fzf' }
 end)
